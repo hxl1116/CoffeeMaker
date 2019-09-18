a861077166e8d63d19b539564d051f25cf8cab4b
Henry Larson <hxl1116@g.rit.edu>
2019-09-18 15:32:35 -0400

    Added test classes.

---
+++ src/test/java/InventoryTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.*;
+
+public class InventoryTest {
+    @BeforeAll
+    public static void setupClass() {
+
+    }
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+
+    }
+}
