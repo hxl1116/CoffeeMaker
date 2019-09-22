cffc04c9fabd19f1d60d5b56e0db87be15bd2af8
Henry Larson <hxl1116@g.rit.edu>
2019-09-22 14:57:41 -0400

    Added addMilk, getSugar, setSugar, and addSugar tests to InventoryTest. Added Mockito dependency to pom.

--- src/main/java/RecipeBook.java
+++ src/main/java/RecipeBook.java
@@ -1,91 +1,102 @@
 public class RecipeBook {
-	
-	/** Array of recipes in coffee maker*/
-	private Recipe [] recipeArray;
-	/** Number of recipes in coffee maker */
-	public static final int NUM_RECIPES = 4;
-	
-	/**
-	 * Default constructor for a RecipeBook.
-	 */
-	public RecipeBook() {
-		recipeArray = new Recipe[NUM_RECIPES];
-	}
-	
-	/**
-	 * Returns the recipe array.
-	 * @return Recipe[]
-	 */
-	public synchronized Recipe[] getRecipes() {
-		return recipeArray;
-	}
-	
-	public synchronized boolean addRecipe(Recipe r) {
 
-		//Assume that the recipe is not null.
-		if(r==null){ return false; }
+    /**
+     * Array of recipes in coffee maker
+     */
+    private Recipe[] recipeArray;
+    /**
+     * Number of recipes in coffee maker
+     */
+    public static final int NUM_RECIPES = 4;
 
-		//Assume recipe doesn't exist in the array until 
-		//find out otherwise
-		boolean exists = false;
-		//Check that recipe doesn't already exist in array
-		for (int i = 0; i < recipeArray.length; i++ ) {
-			if (r.equals(recipeArray[i])) {
-				exists = true;
-			}
-		}
-		//Assume recipe cannot be added until find an empty
-		//spot
-		boolean added = false;
-		//Check for first empty spot in array
-		if (!exists) {
-			for (int i = 0; i < recipeArray.length && !added; i++) {
-				if (recipeArray[i] == null) {
-					recipeArray[i] = r;
-					added = true;
-				}
-			}
-		}
-		return added;
-	}
+    /**
+     * Default constructor for a RecipeBook.
+     */
+    public RecipeBook() {
+        recipeArray = new Recipe[NUM_RECIPES];
+    }
 
-	/**
-	 * Returns the name of the recipe deleted at the position specified
-	 * and null if the recipe does not exist.
-	 * @param recipeToDelete
-	 * @return String
-	 */
-	public synchronized String deleteRecipe(int recipeToDelete) {
+    /**
+     * Returns the recipe array.
+     *
+     * @return Recipe[]
+     */
+    public synchronized Recipe[] getRecipes() {
+        return recipeArray;
+    }
 
-	    // Assumes that the recipeToDelete isn't outside the bounds of the
+    public synchronized boolean addRecipe(Recipe r) {
+
+        //Assume that the recipe is not null.
+        if (r == null) {
+            return false;
+        }
+
+        //Assume recipe doesn't exist in the array until
+        //find out otherwise
+        boolean exists = false;
+        //Check that recipe doesn't already exist in array
+        for (int i = 0; i < recipeArray.length; i++) {
+            if (r.equals(recipeArray[i])) {
+                exists = true;
+            }
+        }
+        //Assume recipe cannot be added until find an empty
+        //spot
+        boolean added = false;
+        //Check for first empty spot in array
+        if (!exists) {
+            for (int i = 0; i < recipeArray.length && !added; i++) {
+                if (recipeArray[i] == null) {
+                    recipeArray[i] = r;
+                    added = true;
+                }
+            }
+        }
+        return added;
+    }
+
+    /**
+     * Returns the name of the recipe deleted at the position specified
+     * and null if the recipe does not exist.
+     *
+     * @param recipeToDelete
+     * @return String
+     */
+    public synchronized String deleteRecipe(int recipeToDelete) {
+
+        // Assumes that the recipeToDelete isn't outside the bounds of the
         // recipe book array.
-	    if (recipeToDelete >= NUM_RECIPES || recipeToDelete < 0){ return null;}
+        if (recipeToDelete >= NUM_RECIPES || recipeToDelete < 0) {
+            return null;
+        }
 
-		if (recipeArray[recipeToDelete] != null) {
-			String recipeName = recipeArray[recipeToDelete].getName();
-			recipeArray[recipeToDelete] = null;
-			return recipeName;
-		} else {
-			return null;
-		}
-	}
-	
-	/**
-	 * Returns the name of the recipe edited at the position specified
-	 * and null if the recipe does not exist.
-	 * @param recipeToEdit
-	 * @param newRecipe
-	 * @return String
-	 */
-	public synchronized String editRecipe(int recipeToEdit, Recipe newRecipe) {
-		if (recipeArray[recipeToEdit] != null) {
-			String recipeName = recipeArray[recipeToEdit].getName();
-			newRecipe.setName("");
-			recipeArray[recipeToEdit] = newRecipe;
-			return recipeName;
-		} else {
-			return null;
-		}
-	}
+        if (recipeArray[recipeToDelete] != null) {
+            String recipeName = recipeArray[recipeToDelete].getName();
+            recipeArray[recipeToDelete] = null;
+            return recipeName;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the name of the recipe edited at the position specified
+     * and null if the recipe does not exist.
+     *
+     * @param recipeToEdit
+     * @param newRecipe
+     * @return String
+     */
+    public synchronized String editRecipe(int recipeToEdit, Recipe newRecipe) {
+        if (recipeArray[recipeToEdit] != null) {
+            String recipeName = recipeArray[recipeToEdit].getName();
+            newRecipe.setName("");
+            recipeArray[recipeToEdit] = newRecipe;
+            return recipeName;
+        } else {
+            return null;
+        }
+    }
 
 }
--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -1,28 +1,235 @@
+import exceptions.InventoryException;
 import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.internal.matchers.Null;
 
-public class InventoryTest {
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class InventoryTest {
+    private static final int DEFAULT_RESOURCE_AMT = 15;
+    private static final int LOW_RESOURCE_AMT = 5;
+    private static final int HIGH_RESOURCE_AMT = 20;
+    private static final String MILK_EXCEPTION_MSG = "Units of milk must be a positive integer";
+    private static final String SUGAR_EXCEPTION_MSG = "Units of sugar must be a positive integer";
+    private static final String COFFEE_AMT_INT = "5";
+    private static final String MILK_AMT_INT = "5";
+    private static final String MILK_AMT_FLOAT = "5.5";
+    private static final String SUGAR_AMT_INT = "5";
+    private static final String SUGAR_AMT_FLOAT = "5.5";
+    private static final String CHOCOLATE_AMT_INT = "5";
+
+    private static Inventory inventory;
+
+    @Mock
+    private static Recipe recipe;
+
     @BeforeAll
-    public static void setupClass() {
+    static void setupClass() {
 
     }
 
     @BeforeEach
-    public void setup() {
+    void setup() {
+        inventory = new Inventory();
+        recipe = mock(Recipe.class);
+    }
+
+    @Test
+    void testAddMilkWithPositiveInteger() {
+        try {
+            inventory.addMilk(MILK_AMT_INT);
+        } catch (InventoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testAddMilkWithNegativeInteger() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addMilk("-" + MILK_AMT_INT);
+        });
+
+        assertEquals(MILK_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddMilkWithPositiveFloat() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addMilk(MILK_AMT_FLOAT);
+        });
+
+        assertEquals(MILK_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddMilkWithNegativeFloat() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addMilk("-" + MILK_AMT_FLOAT);
+        });
+
+        assertEquals(MILK_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddMilkWithNull() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addMilk(null);
+        });
+
+        assertEquals(MILK_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddMilkOnMultipleThreads() {
 
     }
 
     @Test
-    public void test() {
+    void testGetSugar() {
+        assertEquals(DEFAULT_RESOURCE_AMT, inventory.getSugar(), "Should be " + DEFAULT_RESOURCE_AMT);
+    }
 
+    @Test
+    void testSetSugarWithPositiveInteger() {
+        inventory.setSugar(Integer.parseInt(SUGAR_AMT_INT));
+    }
+
+    @Test
+    void testSetSugarWithNegativeInteger() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.setSugar(Integer.parseInt("-" + SUGAR_AMT_INT));
+        });
+
+        assertEquals(SUGAR_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testSetSugarOnMultipleThreads() {
+
+    }
+
+    @Test
+    void testAddSugarWithPositiveInteger() {
+        try {
+            inventory.addSugar(SUGAR_AMT_INT);
+        } catch (InventoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testAddSugarWithNegativeInteger() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addSugar("-" + SUGAR_AMT_INT);
+        });
+
+        assertEquals(SUGAR_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddSugarWithPositiveFloat() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addSugar(SUGAR_AMT_FLOAT);
+        });
+
+        assertEquals(SUGAR_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddSugarWithNegativeFloat() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addSugar("-" + SUGAR_AMT_FLOAT);
+        });
+
+        assertEquals(SUGAR_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddSugarWithNull() {
+        Exception exception = assertThrows(InventoryException.class, () -> {
+            inventory.addSugar(null);
+        });
+
+        assertEquals(SUGAR_EXCEPTION_MSG, exception.getMessage());
+    }
+
+    @Test
+    void testAddSugarOnMultipleThreads() {
+
+    }
+
+    @Test
+    void testEnoughIngredientsSufficientAll() {
+        when(recipe.getAmtCoffee()).thenReturn(DEFAULT_RESOURCE_AMT);
+        when(recipe.getAmtMilk()).thenReturn(DEFAULT_RESOURCE_AMT);
+        when(recipe.getAmtSugar()).thenReturn(DEFAULT_RESOURCE_AMT);
+        when(recipe.getAmtChocolate()).thenReturn(DEFAULT_RESOURCE_AMT);
+
+        assertTrue(inventory.enoughIngredients(recipe));
+    }
+
+    @Test
+    void testEnoughIngredientsNotEnoughCoffee() {
+        when(recipe.getAmtCoffee()).thenReturn(HIGH_RESOURCE_AMT);
+        when(recipe.getAmtMilk()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtSugar()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtChocolate()).thenReturn(LOW_RESOURCE_AMT);
+
+        assertFalse(inventory.enoughIngredients(recipe));
+    }
+
+    @Test
+    void testEnoughIngredientsNotEnoughMilk() {
+        when(recipe.getAmtCoffee()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtMilk()).thenReturn(HIGH_RESOURCE_AMT);
+        when(recipe.getAmtSugar()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtChocolate()).thenReturn(LOW_RESOURCE_AMT);
+
+        assertFalse(inventory.enoughIngredients(recipe));
+    }
+
+    @Test
+    void testEnoughIngredientsNotEnoughSugar() {
+        when(recipe.getAmtCoffee()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtMilk()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtSugar()).thenReturn(HIGH_RESOURCE_AMT);
+        when(recipe.getAmtChocolate()).thenReturn(LOW_RESOURCE_AMT);
+
+        assertFalse(inventory.enoughIngredients(recipe));
+    }
+
+    @Test
+    void testEnoughIngredientsNotEnoughChocolate() {
+        when(recipe.getAmtCoffee()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtMilk()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtSugar()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtChocolate()).thenReturn(HIGH_RESOURCE_AMT);
+
+        assertFalse(inventory.enoughIngredients(recipe));
+    }
+
+    @Test
+    @SuppressWarnings("ConstantConditions")
+    void testEnoughIngredientsWithNull() {
+        when(recipe.getAmtCoffee()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtMilk()).thenReturn(HIGH_RESOURCE_AMT);
+        when(recipe.getAmtSugar()).thenReturn(LOW_RESOURCE_AMT);
+        when(recipe.getAmtChocolate()).thenReturn(LOW_RESOURCE_AMT);
+
+        assertFalse(inventory.enoughIngredients(null));
     }
 
     @AfterEach
-    public void tearDown() {
-
+    void tearDown() {
+        inventory = null;
     }
 
     @AfterAll
-    public static void tearDownClass() {
+    static void tearDownClass() {
 
     }
 }
