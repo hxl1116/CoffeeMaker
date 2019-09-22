bd0d852cdee071703a230fee1610048d3e52ac60
Alex Lamarche <ajl3545@rit.edu>
2019-09-22 14:08:35 -0400

    Added some more tests for RecipeBookTest.
    Fixed the deleteRecipe method remove the recipe and replace it with
    null instead of an empty Recipe. By adding an empty recipe to the recipe
    book, then the string "1." appears when you list all of the current
    recipes.

--- src/test/java/RecipeBookTest.java
+++ src/test/java/RecipeBookTest.java
@@ -54,6 +54,66 @@ public class RecipeBookTest {
         assert(recipeBook.getRecipes()[0].equals(r));
     }
 
+    /**
+     * Full recipe book should not allow for new recipes to
+     * be added.
+     */
+    @Test
+    public void addRecipeToFullBook() {
+        for(int i = 0; i < RecipeBook.NUM_RECIPES; i++){
+            Recipe r = new Recipe();
+            r.setName(""+i);
+            recipeBook.addRecipe(r);
+        }
+
+        Recipe newRecipe = new Recipe();
+        newRecipe.setName("new");
+        boolean isAdded = recipeBook.addRecipe(newRecipe);
+
+        assert(!isAdded);
+
+        // None of the recipes should have the name "new"
+        // if they do, then that means the above recipe was added.
+        for(Recipe r : recipeBook.getRecipes()){
+            assert(r.getName().equals("new") == false);
+        }
+    }
+
+    /**
+     * Should not be able to add a duplicate recipe
+     */
+    @Test
+    public void addDuplicateRecipeToBook() {
+        Recipe r = new Recipe();
+        recipeBook.addRecipe(r);
+        boolean isAdded = recipeBook.addRecipe(r);
+        assert(!isAdded);
+    }
+
+    /**
+     * Should return element from non-empty list of recipes.
+     */
+    @Test
+    public void getRecipeReturnsElements() {
+        Recipe r = new Recipe();
+        r.setName("alex");
+        recipeBook.addRecipe(r);
+
+        assert(recipeBook.getRecipes()[0].getName().equals("alex"));
+    }
+
+    /**
+     * Should remove a recipe from the recipe book and returns its name.
+     */
+    @Test
+    public void deleteRecipeReturnsName() {
+        Recipe r = new Recipe();
+        r.setName("alex");
+        recipeBook.addRecipe(r);
+
+        assert(recipeBook.deleteRecipe(0).equals("alex"));
+        assert(recipeBook.getRecipes()[0] == null);
+    }
 
 
     @AfterEach
