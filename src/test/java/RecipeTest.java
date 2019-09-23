bc2b915c3f853cd557721535a81d480534c20d2c
Noah <nxb9626@rit.edu>
2019-09-22 20:52:02 -0400

    added my tests to RecipeTest

--- src/test/java/RecipeTest.java
+++ src/test/java/RecipeTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RecipeTest {
+    Recipe recp;
     @BeforeAll
     public static void setupClass() {
 
@@ -10,7 +11,213 @@ public class RecipeTest {
 
     @BeforeEach
     public void setup() {
+        this.recp = new Recipe();
+    }
 
+    @Test
+    public void testConstructorChocolate() {
+        assertEquals(recp.getAmtChocolate(), 0, "amtChocolate initialized incorrectly");
+    }
+    @Test
+    public void testConstructorMilk() {
+        assertEquals(recp.getAmtMilk(), 0, "amtMilk initialized incorrectly");
+    }
+    @Test
+    public void testConstructorSugar() {
+        assertEquals(recp.getAmtSugar(), 0, "amtSugar initialized incorrectly");
+    }
+    @Test
+    public void testConstructorCoffee() {
+        assertEquals(recp.getAmtCoffee(), 0, "amtCoffee initialized incorrectly");
+    }
+    @Test
+    public void testConstructorName() {
+        assertEquals(recp.getName(), "", "amtChocolate initialized incorrectly");
+    }
+    @Test
+    public void testConctructorPrice() {
+        assertEquals(recp.getPrice(), 0, "amtChocolate initialized incorrectly");
+    }
+    /*
+    Chocolate
+     */
+    @Test
+    public void testSetAmtChocolatecCatchInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtChocolate("5a");
+        });
+    }
+    @Test
+    public void testSetAmtChocolateCatchnNegativeInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtChocolate("-5");
+        });
+    }
+    @Test
+    public void testSetAmtChocolateValid(){
+        try{
+            recp.setAmtChocolate("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtChocolate());
+    }
+    @Test
+    public void testGetAmtChocolateValid(){
+        try{
+            recp.setAmtChocolate("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtChocolate());
+    }
+    /*
+    Coffee
+     */
+
+    @Test
+    public void testSetAmtCoffeeCatchInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtCoffee("5a");
+        });
+    }
+    @Test
+    public void testSetAmtCoffeeCatchNegativeInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtCoffee("-5");
+        });
+    }
+    @Test
+    public void testSetAmtCoffeeValid(){
+        try{
+            recp.setAmtCoffee("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtCoffee());
+    }
+    @Test
+    public void testGetAmtCoffeeValid(){
+        try{
+            recp.setAmtCoffee("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtCoffee());
+    }
+    /*
+    Milk
+     */
+    @Test
+    public void testSetAmtMilkCatchInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtMilk("5a");
+        });
+    }
+    @Test
+    public void testSetAmtMilkCatchNegativeInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtMilk("-5");
+        });
+    }
+    @Test
+    public void testSetAmtMilkValid(){
+        try{
+            recp.setAmtMilk("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtMilk());
+    }
+    @Test
+    public void testGetAmtMilkValid(){
+        try{
+            recp.setAmtMilk("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtMilk());
+    }
+    /*
+    Sugar
+     */
+    @Test
+    public void testSetAmtSugarCatchInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtSugar("5a");
+        });
+    }
+    @Test
+    public void testSetAmtSugarCatchNegativeInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setAmtSugar("-5");
+        });
+    }
+    @Test
+    public void testSetAmtSugarValid(){
+        try{
+            recp.setAmtSugar("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtSugar());
+    }
+    @Test
+    public void testGetAmtSugarValid(){
+        try{
+            recp.setAmtSugar("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getAmtSugar());
+    }
+    /*
+        name
+     */
+    @Test
+    public void testSetName(){
+        recp.setName("testName");
+        assertEquals("testName", recp.getName());
+    }
+    @Test
+    public void testGetName(){
+        recp.setName("testName");
+        assertEquals("testName", recp.getName());
+    }
+    /*
+    price
+     */
+    @Test
+    public void testSetPriceCatchInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setPrice("5a");
+        });
+    }
+    @Test
+    public void testSetPriceCatchNegativeInt(){
+        assertThrows(RecipeException.class, ()->{
+            recp.setPrice("-5");
+        });
+    }
+    @Test
+    public void testSetPriceValid(){
+        try{
+            recp.setPrice("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getPrice());
+    }
+    @Test
+    public void testGetPriceValid(){
+        try{
+            recp.setPrice("5");
+        }
+        catch(Exception e){}
+        assertEquals(5, recp.getPrice());
+    }
+    @Test
+    public void testHashWithName(){
+        int actual = 31;
+        assertEquals(actual, recp.hashCode());
+    }
+    @Test
+    public void testHashWithNoName(){
+        String name = "hello there";
+        recp.setName(name);
+        int actual = name.hashCode() + 31;
+        assertEquals(actual, recp.hashCode());
     }
 
     @Test
