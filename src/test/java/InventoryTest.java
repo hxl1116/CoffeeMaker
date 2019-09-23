f60c2814e3cb7e143dd37995856e6c461c431aea
SWilson333 <43420037+SWilson333@users.noreply.github.com>
2019-09-22 20:37:14 -0400

    Added all the tests for inventory

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -22,6 +22,8 @@ class InventoryTest {
     private static final String SUGAR_AMT_FLOAT = "5.5";
     private static final String CHOCOLATE_AMT_INT = "5";
 
+    private int default_amount = 15;
+
     private static Inventory inventory;
 
     @Mock
@@ -222,6 +224,166 @@ class InventoryTest {
 
         assertFalse(inventory.enoughIngredients(null));
     }
+    @Test
+    public void testInventory() {
+        assertNotNull(inventory, "Inventory not created.");
+    }
+    @Test
+    public void testGetChocolate() {
+        assertEquals(default_amount,inventory.getChocolate(), "The inventory did not start with the default amount of chocolate.");
+    }
+    @Test
+    public void testGetChocolateNo() {
+        assertNotSame(default_amount+1,inventory.getChocolate(),"The inventory start with the default amount of chocolate.");
+    }
+    @Test
+    public void testSetPositiveChocolate(){
+        int new_amount = 10;
+        inventory.setChocolate(new_amount);
+        int testAmount = 0;
+
+        assertEquals(new_amount,testAmount,"The amount of chocolate in the inventory is incorrect.");
+    }
+    @Test
+    public void testSetZeroChocolate() {
+        int new_amount = 0;
+        inventory.setChocolate(new_amount);
+        int testAmount = 0;
+
+        assertEquals(new_amount,testAmount,"The amount of chocolate in the inventory is incorrect.");
+    }
+    @Test
+    public void testSetNegativeChocolate(){
+        int testAmount = 0;
+        int negative_amount = -1;
+        inventory.setChocolate(negative_amount);
+
+        assertEquals(default_amount, testAmount, "Amount of chocolate was set while trying to add a negative amount.");
+    }
+    @Test
+    public void testAddPositiveChocolate(){
+        int add = 10;
+        inventory.setChocolate(default_amount);
+        assertEquals(default_amount + add, inventory.getChocolate(),"Amount of chocolate not increased.");
+    }
+    @Test
+    public void testAddNegativeChocolate(){
+        inventory.setChocolate(default_amount);
+        boolean NegFlag = false;
+        String neg = "-1";
+        try {
+            inventory.addChocolate(neg);
+        }
+        catch (InventoryException e) {
+            NegFlag = true;
+        }
+
+        assertTrue(NegFlag,"Exception not thrown for adding a negative amount of chocolate");
+    }
+    @Test
+    public void testAddNotANumberChocolate(){
+        inventory.setChocolate(default_amount);
+        boolean NanFlag = false;
+        String neg = "a";
+        try {
+            inventory.addChocolate(neg);
+        }
+        catch (InventoryException e) {
+            NanFlag = true;
+        }
+        assertTrue(NanFlag, "Exception not thrown for adding a letters worth of chocolate.");
+    }
+    @Test
+    public void testGetCoffee() {
+        assertEquals( default_amount, inventory.getCoffee(), "Incorrect default amount of coffee." );
+        }
+
+    @Test
+    public void testGetCoffeeNo() {
+        assertNotSame(default_amount+1,inventory.getCoffee(),"The inventory start with the default amount of Coffee.");
+    }
+    @Test
+    public void testSetPositiveCoffee(){
+        int new_amount = 10;
+        inventory.setCoffee(new_amount);
+        int testAmount = 0;
+
+        assertEquals( new_amount, testAmount,"Amount of coffee not set correctly.");
+    }
+    @Test
+    public void testSetZeroCoffee(){
+        int new_amount = 0;
+        inventory.setCoffee(new_amount);
+        int testAmount = 0;
+
+        assertEquals( new_amount, testAmount,"Amount of coffee not set correctly.");
+    }
+    @Test
+    public void testSetNegativeCoffee(){
+        int newAmount = -1;
+        inventory.setCoffee(newAmount);
+        int testAmount = 0;
+
+        assertEquals(default_amount, testAmount, "Negative amount of coffee set.");
+    }
+    @Test
+    public void testAddPositiveCoffee(){
+        int add =  5;
+        inventory.setCoffee(default_amount);
+        try {
+            inventory.addCoffee(String.valueOf(add));
+        } catch(InventoryException e) {
+            //TODO Auto created Catch
+        }
+        assertEquals(default_amount+add, inventory.getCoffee(),"Coffee amount not correct");
+    }
+    @Test
+    public void testAddCoffeeNonInt(){
+        boolean LetterFlag = false; //Checks if letter tries to be added
+        inventory.setCoffee(default_amount);
+        try {
+            inventory.addCoffee("a");
+        } catch(InventoryException e) {
+            LetterFlag = true;
+        }
+        assertTrue(LetterFlag, "Exception not thrown for adding a letter instead of a positive number.");
+    }
+    @Test
+    public void testAddCoffeeNegative(){
+        int add = -20;
+        boolean NegativeFlag = false; //Checks if negative number tries to be added
+        inventory.setCoffee(default_amount);
+        try {
+            inventory.addCoffee(String.valueOf(add));
+        } catch(InventoryException e) {
+            NegativeFlag = true;
+        }
+        assertTrue(NegativeFlag, "Exception not thrown for adding a negative number instead of a positive number.");
+    }
+    @Test
+    public void testGetMilk(){
+        assertEquals(default_amount, inventory.getMilk(),"Wrong amount of Milk.");
+    }
+    @Test
+    public void testGetMilkNo(){
+        assertNotSame(default_amount+1,inventory.getMilk(), "The inventory start with the default amount of Milk.");
+    }
+    @Test
+    public void testSetMilkPos(){
+        int pos_amount = 10;
+        inventory.setMilk(pos_amount);
+        int testAmount = 0;
+
+        assertEquals( pos_amount, testAmount, "Amount of coffee not set correctly.");
+    }
+    @Test
+    public void testSetMilkNeg(){
+        int neg_amount = -10;
+        inventory.setMilk(neg_amount);
+        int testAmount = 0;
+
+        assertNotSame(neg_amount, testAmount, "Negative amount of coffee set.");
+    }
 
     @AfterEach
     void tearDown() {
@@ -232,4 +394,8 @@ class InventoryTest {
     static void tearDownClass() {
 
     }
+
+
+
+
 }
